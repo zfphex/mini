9159038c27a1ba19ab8abf536b3f2a44e13e4d44
zX3no <28522740+zX3no@users.noreply.github.com>
2023-10-20 22:20:41 +1030

    Removed padding from log and warn

--- src/lib.rs
+++ src/lib.rs
@@ -54,7 +54,7 @@ macro_rules! info {
         #[cfg(not(feature = "error"))]
         #[cfg(not(feature = "warn"))]
         {
-            print!("\x1b[90m{} \x1b[92mINFO\x1b[0m  {}:\x1b[30m{}\x1b[0m - ", $crate::now(), file!(), line!());
+            print!("\x1b[90m{} \x1b[92mINFO\x1b[0m {}:\x1b[30m{}\x1b[0m - ", $crate::now(), file!(), line!());
             println!($($arg)*);
         }
     };
@@ -66,7 +66,7 @@ macro_rules! warn {
         #[cfg(not(feature = "strip"))]
         #[cfg(not(feature = "error"))]
         {
-            print!("\x1b[90m{} \x1b[93mWARN\x1b[0m  {}:\x1b[30m{}\x1b[0m - ", $crate::now(), file!(), line!());
+            print!("\x1b[90m{} \x1b[93mWARN\x1b[0m {}:\x1b[30m{}\x1b[0m - ", $crate::now(), file!(), line!());
             println!($($arg)*);
         }
     };
