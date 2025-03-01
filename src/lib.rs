7287c86f7503ead33a9e7fae1b112db0fdee652d
Bay <28522740+zX3no@users.noreply.github.com>
2025-03-01 10:43:29 +1030

    removed trash

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,9 @@ mod args;
 mod log;
 mod profile;
 mod reflect;
-mod trash;
 
 pub use ansi::*;
 pub use args::*;
 pub use log::*;
 pub use profile::*;
 pub use reflect::*;
-
-#[cfg(target_os = "windows")]
-pub use trash::windows::*;
